f5ba336c79e27409530a2a1f2d9c66c0cb0bcc1f
Tedde6756 <theodorplahn@elev.ga.lbs.se>
2026-01-12 14:41:49 +0100

    l

--- programveckor/Assets/bullet.cs
+++ programveckor/Assets/bullet.cs
@@ -2,19 +2,23 @@ using UnityEngine;
 
 public class bullet : MonoBehaviour
 {
+
+    private Vector3 mousePos;
+    private Camera mainCam;
+    private Rigidbody2D rb;
+    public float force;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    private void OnCollisionEnter2D(Collision2D Collision)
+    void Start()
     {
-        Destroy(gameObject);   
-       
-        
-        if (Collision.gameObject.TryGetComponent<enemyHP>(out enemyHP enemyComponent)) 
-        {
-            enemyComponent.TakeDamage(1);
-        }
-
-        Destroy(gameObject);
-
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        rb = GetComponent<Rigidbody2D>();
+        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 direction = mousePos - transform.position;
+        Vector3 rotation = transform.position - mousePos;
+        rb.linearVelocity = new Vector2 (direction.x, direction.y).normalized * force;
+        float rot = Mathf.Atan2(rotation.x, rotation.y) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rot + 90);
     }
 
     // Update is called once per frame
